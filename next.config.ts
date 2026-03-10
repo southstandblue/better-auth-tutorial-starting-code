30ca88dcb7f405b33a6181b3b42e33e9c23bdc6b
NickC <southstandblue@gmail.com>
2026-03-10 10:59:48 +0000

    Minor Tweaks

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
+// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
+  // webpack: (config, { isServer }) => {
+  //   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  //   if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()];
+  //   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  //   return config;
+  // },
   experimental: {
     authInterrupts: true,
   },
